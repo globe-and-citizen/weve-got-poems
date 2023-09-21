06bf4c6257a33834bd12ad4b87632689cfa8f9bb
Arnon <arnonrdp@gmail.com>
2023-09-21 18:17:36 -0300

    Created `userRoutes`

---
+++ backend/src/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/userController')
+
+router.post('/user', userController.create)
+router.get('/users', userController.read)
+router.put('/user/:id', userController.update)
+router.delete('/user/:id', userController.remove)
+
+module.exports = router
