e4f2252ba847789b1e2a07a233150437d22b8053
Arnon <arnonrdp@gmail.com>
2023-09-23 09:32:46 -0300

    Added `/login` route

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -6,5 +6,6 @@ router.post('/user', userController.create)
 router.get('/users', userController.read)
 router.put('/user/:id', userController.update)
 router.delete('/user/:id', userController.remove)
+router.post('/login', userController.login)
 
 module.exports = router
