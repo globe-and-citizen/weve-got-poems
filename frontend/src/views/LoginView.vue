992c6361797a905ea62f74a5edac850072aeedbe
Hermann EHO <hermannleboss@gmail.com>
2023-09-27 08:44:11 +0000

    build: sass and import

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an Login page</h1>
+    <h1 class='bg-amber-400'>This is an Login page</h1>
   </div>
 </template>
 
-<style>
+<style lang='scss'>
 @media (min-width: 1024px) {
   .about {
   }
