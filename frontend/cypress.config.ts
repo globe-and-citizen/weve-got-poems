f5dde57fb3bb4c6cdd1ea4908913c48f545294e9
Hermann EHO <hermannleboss@gmail.com>
2023-09-27 12:47:08 +0000

    feat: config cypress

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'cypress'
-// export default defineConfig({
-// })
+import { defineConfig } from "cypress";
 export default defineConfig({
   component: {
     devServer: {
-      framework: 'vue',
-      bundler: 'vite',
+      framework: "vue",
+      bundler: "vite",
     },
   },
+
   e2e: {
-    baseUrl: 'http://localhost:1234',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
-})
+});
