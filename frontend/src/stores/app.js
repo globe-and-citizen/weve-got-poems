d2734086218248c5b9bb6ac8b75ec545bd52af92
Hermann EHO <hermannleboss@gmail.com>
2023-09-27 09:58:16 +0000

    feat: handle token expiration

--- frontend/src/stores/app.js
+++ frontend/src/stores/app.js
@@ -1,12 +1,16 @@
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', () => {
   const token = ref('')
+  const tokenExpiration = ref(0)
 
   function setToken(newToken) {
+    tokenExpiration.value = Date.now() + 1000 * 60 * 60
     token.value = newToken
   }
 
-  return { token, setToken }
+  const getToken = computed(() => tokenExpiration > Date.now() ? token.value : '')
+
+  return { token, setToken, getToken }
 })
