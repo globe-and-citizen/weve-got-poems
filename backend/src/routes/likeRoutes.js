2424db26479ba06df0062dcaa1c44c52177b4447
Arnon <arnonrdp@gmail.com>
2023-09-28 18:47:14 -0300

    Added route to remove like or dislike

--- backend/src/routes/likeRoutes.js
+++ backend/src/routes/likeRoutes.js
@@ -4,5 +4,7 @@ const likesController = require('../controllers/likesController')
 
 router.post('/like', likesController.create)
 router.post('/dislike', likesController.create)
+router.delete('/like/:id', likesController.remove)
+router.delete('/dislike/:id', likesController.remove)
 
 module.exports = router
