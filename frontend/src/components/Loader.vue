251529535bd0b5cb5bde1416aa898c18767eea0a
Hermann EHO <hermannleboss@gmail.com>
2023-10-02 15:57:39 +0000

    test: add data-test

--- frontend/src/components/Loader.vue
+++ frontend/src/components/Loader.vue
@@ -1,5 +1,5 @@
 <template>
   <div class='w-8 h-8 border-4 border-emerald-400 rounded-full animate-spin'
-       style='border-top-color:transparent'></div>
+       style='border-top-color:transparent' data-test='loader'></div>
 
 </template>
