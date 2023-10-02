755d58dcdd76c159863a399e589a0bbf12e0d7cd
Hermann EHO <hermannleboss@gmail.com>
2023-10-02 20:57:01 +0000

    feat: create user management in the store with a default value

--- frontend/src/stores/app.ts
+++ frontend/src/stores/app.ts
@@ -5,13 +5,27 @@ export const useAppStore = defineStore('app', () => {
   const token = ref('')
   const tokenExpiration = ref(0)
 
-  function setToken(newToken) {
+  // TODO: Remove default value
+  const user = ref(
+    {
+      'id': 7,
+      'name': 'tester account',
+      'email': 'test@doe.com',
+      'created_at': '2023-09-26T11:13:15.861Z'
+    })
+
+  function setToken(newToken: string) {
     tokenExpiration.value = Date.now() + 1000 * 60 * 60
     token.value = newToken
   }
 
+  const setUser = (user: any) => {
+    user.value = user
+  }
+
 
   const getToken = computed(() => tokenExpiration.value > Date.now() ? token.value : '')
+  const getUser = computed(() => user.value)
 
-  return { token,tokenExpiration, setToken, getToken }
+  return { token, tokenExpiration, setToken, getToken, getUser, setUser }
 }, { persist: true })
