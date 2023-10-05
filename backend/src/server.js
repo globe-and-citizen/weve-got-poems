6c574ca7ad17edc736c4509f6f792e92ae83fc0b
Arnon R <arnonrdp@gmail.com>
2023-10-05 14:34:29 -0300

    Imported authRoutes into `/v1` route

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use(layer8)
 
+const authRoutes = require('./routes/authRoutes')
 const configRoutes = require('./routes/configRoutes')
 const likeRoutes = require('./routes/likeRoutes')
 const poemRoutes = require('./routes/poemRoutes')
@@ -17,7 +18,7 @@ const userRoutes = require('./routes/userRoutes')
 const swaggerRoute = require('./routes/swaggerRoute')
 
 app.use('/', swaggerRoute)
-app.use('/v1', likeRoutes, poemRoutes, userRoutes)
+app.use('/v1', authRoutes, likeRoutes, poemRoutes, userRoutes)
 app.use('/config', configRoutes) // Uncomment if you want to use the config routes
 
 app.listen(port, () => {
