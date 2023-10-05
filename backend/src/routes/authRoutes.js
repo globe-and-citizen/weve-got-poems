89d74755d36759fcb7dc44615a11873e50063ec4
Arnon R <arnonrdp@gmail.com>
2023-10-05 15:21:30 -0300

    Fixed const name

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
 
-router.post('/login', userController.login)
+router.post('/login', authController.login)
 router.get('/nonce', authController.nonce)
 router.post('/verify', authController.verify)
 
