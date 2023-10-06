d84aaa5c4066bf672642631bc90832425175e0e6
Hermann EHO <hermannleboss@gmail.com>
2023-10-06 13:03:45 +0000

    feat: useFetch Delay

--- frontend/src/composables/useFetch.ts
+++ frontend/src/composables/useFetch.ts
@@ -6,6 +6,7 @@ interface FetchResult<T> {
   error: Ref<String | null>;
   loading: Ref<boolean>;
   isLoaded: Ref<boolean>;
+  execute?: () => void;
 }
 
 export function useFetch<T>(url: string, option: RequestInit = {}): FetchResult<T> {
@@ -33,4 +34,33 @@ export function useFetch<T>(url: string, option: RequestInit = {}): FetchResult<
     })
 
   return { data, error, loading, isLoaded }
+}
+
+
+export function useFetchDelay<T>(url: string, option: RequestInit = {}): FetchResult<T> {
+  const data = ref<T | null>(null)
+  const error = ref<String | null>(null)
+  const loading = ref<boolean>(true)
+  const isLoaded = ref<boolean>(false)
+  const execute = () => {
+    fetch(url, option)
+      .then(async (response) => {
+        if (response.ok) {
+          data.value = await response.json()
+        } else if (response.status >= 400 && response.status < 500) {
+          error.value = `Client Error: ${response.status} - ${response.statusText}`
+        } else {
+          error.value = `Server Error: ${response.status} - ${response.statusText}`
+        }
+      })
+      .catch((err) => {
+        error.value = `Network Error: ${err.message}`
+      })
+      .finally(() => {
+        isLoaded.value = true
+        loading.value = false
+      })
+  }
+
+  return { data, error, loading, isLoaded, execute }
 }
--- frontend/src/views/PoemView.vue
+++ frontend/src/views/PoemView.vue
@@ -1,21 +1,42 @@
 <script setup lang='ts'>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watchEffect } from 'vue'
 import { RouterLink, useRouter } from 'vue-router'
 import DocumentationIcon from '@/components/icons/IconDocumentation.vue'
 import WelcomeItem from '@/components/WelcomeItem.vue'
+import { useAppStore } from '@/stores/app'
+import { useFetch, useFetchDelay } from '@/composables/useFetch'
+import Loader from '@/components/Loader.vue'
 
-
+const endpoint = import.meta.env.VITE_BACKEND_ENDPOINT
 const router = useRouter()
+const appStore = useAppStore()
 
 const data = ref()
 const index = ref()
 const nextId = ref()
 const previousId = ref()
 
-const poem = () => {
+const notification = ref()
+
+const { error, data: poems, loading, isLoaded } = useFetch(endpoint + '/poems/')
+const {
+  error: deleteError,
+  data: deleteData,
+  loading: deleteLoading,
+  execute: executeDelete
+} = useFetchDelay(endpoint + '/poem/' + router.currentRoute.value.params.id, {
+  method: 'DELETE',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + appStore.getToken
+  }
+})
+
+
+const getCurrentPoem = () => {
   if (!data.value) return
-  return data.value.find((poem, i) => {
-    if (poem.id === parseInt(router.currentRoute.value.params.id)) {
+  return data.value.find((poem: any, i: number) => {
+    if (poem.id === parseInt(router.currentRoute.value.params.id as string)) {
       index.value = i
       if (i === 0) {
         previousId.value = data.value[data.value.length - 1].id
@@ -31,27 +52,76 @@ const poem = () => {
     }
   })
 }
+
+const currentPoem = ref()
 onMounted(async () => {
-  const response = await fetch('https://weve-got-poems-server.onrender.com/v1/poems')
+  const response = await fetch(endpoint + '/poems')
   data.value = await response.json()
 })
 
+
+const processDeleteNotification = () => {
+  if (deleteData.value) {
+    notification.value = {
+      message: 'Poem deleted successfully',
+      status: 'success'
+    }
+    // redirect to home page after timeout
+    setTimeout(() => {
+      router.push('/')
+    }, 2000)
+  } else {
+    if (deleteError.value) {
+      notification.value = {
+        message: `Unable to delete poem ${deleteError.value}`,
+        status: 'error'
+      }
+    }
+  }
+}
+
+watchEffect(() => {
+  processDeleteNotification()
+  currentPoem.value = getCurrentPoem()
+  if (deleteError.value) {
+    notification.value = {
+      message: `Unable to delete poem ${deleteError.value}`,
+      status: 'error'
+    }
+  }
+})
+const onDelete = async () => {
+  const confirmation = window.confirm('Are you sure you want to delete this poem?')
+  if (confirmation) {
+    // delete poem
+    const id = currentPoem.value.id
+    deleteLoading.value = true
+    if (executeDelete) {
+      executeDelete()
+    }
+  }
+}
 
 </script>
 
 <template>
   <main>
-    <WelcomeItem v-if='poem()'>
+    <Loader v-if='loading' />
+    <WelcomeItem v-if='currentPoem'>
       <template #icon>
         <DocumentationIcon />
       </template>
       <template #heading>
-        <h1>{{ poem().title }}</h1>
+        <h1>{{ currentPoem.title }}</h1>
       </template>
-      {{ poem().content }}
+      <div class='flex gap-2' v-if='currentPoem.author.id==appStore.getUser.id'>
+        <RouterLink :to="'/poems/' + currentPoem.id+'/edit'">Edit</RouterLink>
+        <a @click.prevent='onDelete()' data-test='remove-poem-button'>Remove</a>
+      </div>
+      {{ currentPoem.content }}
       <br>
       <br>
-      {{ new Date(poem().created_at).toLocaleDateString() }}
+      {{ new Date(currentPoem.created_at).toLocaleDateString() }}
       <div>
         <div class='navigation'>
           <RouterLink :to="'/poems/' + previousId">Prev Poem</RouterLink>
@@ -68,6 +138,4 @@ onMounted(async () => {
   display: flex;
   justify-content: space-between;
 }
-
-
 </style>
