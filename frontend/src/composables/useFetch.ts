73c48d2a8416ce46bc134c25d2c819a1d4116314
Hermann EHO <hermannleboss@gmail.com>
2023-10-06 11:31:39 +0000

    feat: useFetch

---
+++ frontend/src/composables/useFetch.ts
@@ -0,0 +1,35 @@
+import type { Ref, UnwrapRef } from 'vue'
+import { ref } from 'vue'
+
+interface FetchResult<T> {
+  data: Ref<UnwrapRef<T | null>>;
+  error: Ref<String | null>;
+  loading: Ref<boolean>;
+  isLoaded: Ref<boolean>;
+}
+
+export async function useFetch<T>(url: string): Promise<FetchResult<T>> {
+  const data = ref<T | null>(null)
+  const error = ref<String | null>(null)
+  const loading = ref<boolean>(false)
+  const isLoaded = ref<boolean>(false)
+
+  try {
+    loading.value = true
+    const response = await fetch(url)
+    isLoaded.value = true
+    if (response.ok) {
+      data.value = await response.json()
+    } else if (response.status >= 400 && response.status < 500) {
+      error.value = `Client Error: ${response.status} - ${response.statusText}`
+    } else {
+      error.value = `Server Error: ${response.status} - ${response.statusText}`
+    }
+  } catch (err: any) {
+    error.value = `Network Error: ${err.message}`
+  } finally {
+    loading.value = false
+  }
+
+  return { data, error, loading, isLoaded }
+}
