4e93c6bfc4a64c0a746e277d885f9a42ccfeb7d7
Hermann EHO <hermannleboss@gmail.com>
2023-10-10 09:16:19 +0000

    feat: use of jwt_decode to get the user information

--- frontend/src/stores/app.ts
+++ frontend/src/stores/app.ts
@@ -1,31 +1,23 @@
 import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
+import jwt_decode from 'jwt-decode'
 
 export const useAppStore = defineStore('app', () => {
-  const token = ref('')
-  const tokenExpiration = ref(0)
+    const token = ref('')
+    const tokenExpiration = ref(0)
 
-  // TODO: Remove default value
-  const user = ref(
-    {
-      'id': 7,
-      'name': 'tester account',
-      'email': 'test@doe.com',
-      'created_at': '2023-09-26T11:13:15.861Z'
-    })
+    function setToken(newToken: string) {
+        tokenExpiration.value = Date.now() + 1000 * 60 * 60
+        token.value = newToken
+    }
 
-  function setToken(newToken: string) {
-    tokenExpiration.value = Date.now() + 1000 * 60 * 60
-    token.value = newToken
-  }
-
-  const setUser = (user: any) => {
-    user.value = user
-  }
+    const setUser = (user: any) => {
+        user.value = user
+    }
 
 
-  const getToken = computed(() => tokenExpiration.value > Date.now() ? token.value : '')
-  const getUser = computed(() => user.value)
+    const getToken = computed(() => tokenExpiration.value > Date.now() ? token.value : '')
+    const getUser = computed(() => jwt_decode(getToken.value))
 
-  return { token, tokenExpiration, setToken, getToken, getUser, setUser }
+    return { token, tokenExpiration, setToken, getToken, getUser, setUser }
 }, { persist: true })
