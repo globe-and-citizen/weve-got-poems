fe1450e3d3839fc88ed38014213bd7aa23372dda
Hermann EHO <hermannleboss@gmail.com>
2023-10-14 11:51:38 +0000

    feat: catch response.json error for delete

--- frontend/src/composables/useFetch.ts
+++ frontend/src/composables/useFetch.ts
@@ -42,7 +42,11 @@ export function useFetch<T>(url: string, option: RequestInit = {}, useFetchOptio
     fetch(url, option)
       .then(async (response) => {
         if (response.ok) {
-          data.value = await response.json()
+          try {
+            data.value = await response.json()
+          } catch (e) {
+            console.log(e)
+          }
         } else if (response.status >= 400 && response.status < 500) {
           error.value = `Client Error: ${response.status} - ${response.statusText}`
         } else {
