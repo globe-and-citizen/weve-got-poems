df38ab5f66ad6ac074e4c01157b0dedbfee4b94c
Hermann EHO <hermannleboss@gmail.com>
2023-10-15 08:59:07 +0000

    refactor: improve readability

--- frontend/src/composables/useWallet.ts
+++ frontend/src/composables/useWallet.ts
@@ -4,12 +4,15 @@ import { BrowserProvider } from 'ethers'
 export function useWallet() {
     const userAddress = ref()
     const isConnected = ref(false)
-    // const provider: Ref<BrowserProvider | undefined> = ref()
     let provider: BrowserProvider | undefined
     const signer = ref()
     const endpoint = import.meta.env.VITE_BACKEND_ENDPOINT
     let intervalId: string | number | NodeJS.Timeout | undefined
 
+    /**
+     * This function connects the user's MetaMask wallet to the app.
+     * It uses the window.ethereum.request method to connect the wallet.
+     */
     async function connectWallet() {
         try {
             if (typeof window.ethereum !== 'undefined') {
@@ -22,19 +25,21 @@ export function useWallet() {
             }
             provider = new BrowserProvider(window.ethereum)
             if (provider) {
-                console.log('1')
                 signer.value = await provider.getSigner()
-                console.log('2')
+                userAddress.value = await signer.value.getAddress()
             }
-            const accounts = await ethereum.request({
-                method: 'eth_requestAccounts'
-            })
-            userAddress.value = accounts.pop()
         } catch (error) {
             console.log('Error: ', error)
         }
     }
 
+    /**
+     * This is a Vue lifecycle hook that is called when the component is mounted.
+     * We use it to check if the user is connected to MetaMask.
+     * If they are, we set the userAddress and isConnected variables.
+     * We also set up a setInterval to check if the user is still connected.
+     * If they are not, we set the userAddress and isConnected variables to their default values.
+     */
     onMounted(async () => {
         if (window.ethereum) {
             provider = new BrowserProvider(window.ethereum)
@@ -48,6 +53,7 @@ export function useWallet() {
                     if (isConnected.value) {
                         // This throws an error if the user is not connected
                         signer.value = await provider.getSigner()
+                        userAddress.value = await signer.value.getAddress()
                     } else {
                         signer.value = undefined
                     }
@@ -59,6 +65,11 @@ export function useWallet() {
             }
         }, 1000)
     })
+
+    /**
+     * This is a Vue lifecycle hook that is called when the component is unmounted.
+     * We use it to clear the interval that we set up in the onMounted hook.
+     */
     onBeforeUnmount(() => {
         if (intervalId) clearInterval(intervalId)
     })
