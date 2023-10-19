879fc1dac1e5d0b49059a3d60a5566c2dd92ef1c
Hermann EHO <hermannleboss@gmail.com>
2023-10-19 21:29:57 +0000

    feat: handle user information update

--- frontend/src/views/UserView.vue
+++ frontend/src/views/UserView.vue
@@ -6,24 +6,24 @@
       </ac-notification>
     </div>
     <div class='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-      <form class='space-y-6' @submit.prevent='submitLogin'>
+      <form class='space-y-6' @submit.prevent='submitUpdate'>
         <div>
           <label class='block text-sm font-medium leading-6 text-gray-900' for='email'>Email address</label>
           <div class='mt-2'>
-            <input id='email' v-model='loginEmail' autocomplete='email'
+            <input id='email' v-model='email' autocomplete='email'
                    class='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6'
                    name='email' required
-                   type='email'/>
+                   type='email' />
           </div>
         </div>
 
         <div>
           <label class='block text-sm font-medium leading-6 text-gray-900' for='name'>Displayed Name</label>
           <div class='mt-2'>
-            <input id='name' autocomplete='email'
+            <input id='name' v-model='name'
                    class='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6'
                    name='name' required
-                   type='email'/>
+                   type='text' />
           </div>
         </div>
 
@@ -32,19 +32,21 @@
             <label class='block text-sm font-medium leading-6 text-gray-900' for='password'>Password</label>
           </div>
           <div class='mt-2'>
-            <input id='password' v-model='loginPassword' autocomplete='current-password'
+            <input id='password' v-model='password' autocomplete='current-password'
                    class='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6'
                    name='password'
                    required
-                   type='password'/>
+                   type='password' />
           </div>
         </div>
 
         <div>
           <button
-              class='flex w-full justify-center rounded-md bg-emerald-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600'
-              data-test='login-button'
-              type='submit'>
+            class='flex w-full justify-center rounded-md bg-emerald-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600'
+            data-test='login-button'
+            type='submit'>
+
+            <CustomLoader v-if='updateLoading' class='mr-6' />
             Save
           </button>
         </div>
@@ -56,166 +58,100 @@
 </template>
 
 <style lang='scss'>
-* {
-  //border: solid red 1px;
-}
 
-@media (min-width: 1024px) {
-  .about {
-  }
-}
 </style>
 
 <script setup lang='ts'>
 
-import {ref} from 'vue'
-import {useAppStore} from '@/stores/app'
-import {useRouter} from 'vue-router'
+import { ref } from 'vue'
+import { useAppStore } from '@/stores/app'
+import { useRouter } from 'vue-router'
 import AcNotification from '@/components/ac-notification.vue'
-import {useWallet} from '@/composables/useWallet'
+import { useWallet } from '@/composables/useWallet'
+import CustomLoader from '@/components/CustomLoader.vue'
 
 
 const endpoint = import.meta.env.VITE_BACKEND_ENDPOINT
 
 
-const status = ref(true)
-
-const registerEmail = ref('')
-const registerName = ref('')
-const registerPassword = ref('')
-
-const loginEmail = ref('')
-const loginPassword = ref('')
-
-const loginError = ref()
-const loginData = ref()
-const loginStatus = ref()
-const loginLoading = ref()
-const loginLoaded = ref(false)
-
-const registerError = ref()
-const registerData = ref()
-const registerStatus = ref()
-const notification = ref()
-
 const router = useRouter()
 
 const appStore = useAppStore()
+
+const email = ref('')
+const name = ref('')
+const password = ref('')
+
+if (appStore.getUser) {
+  email.value = appStore.getUser.email
+  name.value = appStore.getUser.name
+}
+
+const updateError = ref()
+const updateData = ref()
+const updateLoading = ref(false)
+
+const notification = ref()
+
 const wallet = useWallet()
 const onSignIn = () => {
   wallet.signInWithEthereum()
 }
-const setStatus = (value: boolean) => {
-  status.value = value
-}
 
-const submitLogin = async () => {
+const submitUpdate = async () => {
 
   // Handle fetch in a try catch
   try {
-    loginLoading.value = true
-    const response = await fetch(endpoint + '/login', {
+    updateLoading.value = true
+    const response = await fetch(endpoint + '/user', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        email: loginEmail.value,
-        password: loginPassword.value
+        email: email.value,
+        password: password.value,
+        name: name.value
       })
     })
     if (response.ok) {
       // Successful response
-      loginData.value = await response.json()
-      if (loginData.value.token) {
-        appStore.setToken(loginData.value.token)
+      updateData.value = await response.json()
+      if (updateData.value.token) {
+        appStore.setToken(updateData.value.token)
       }
       // Notification message
       notification.value = {
-        message: 'Welcome to our platform! ',
+        message: 'User Successfully updated! ',
         status: 'success'
       }
-      // Redirection
-      setTimeout(() => {
-        router.push('/')
-      }, 2000)
     } else if (response.status >= 400 && response.status < 500) {
       // Client error
       notification.value = {
-        message: `Unable to login the user  : Client Error: ${response.status} - ${response.statusText}`,
+        message: `Unable to update the user  : Client Error: ${response.status} - ${response.statusText}`,
         status: 'error'
       }
     } else {
       // Server error
       notification.value = {
-        message: `Unable to login the user  : Server Error: ${response.status} - ${response.statusText}`,
+        message: `Unable to update the user  : Server Error: ${response.status} - ${response.statusText}`,
         status: 'error'
       }
     }
   } catch (error: any) {
     // Network error or other unexpected errors
     notification.value = {
-      message: `Unable to login the user  : Unexpected Error: ${error.message}`,
+      message: `Unable to update the user  : Unexpected Error: ${error.message}`,
       status: 'error'
     }
-    loginError.value = error
+    updateError.value = error
   } finally {
-    loginLoading.value = false
+    updateLoading.value = false
   }
 
   // reset inputs values
-  loginEmail.value = ''
-  loginPassword.value = ''
-  loginStatus.value = 'done'
-}
-
-const submitRegister = async () => {
-  // send data to backend using fetch api
-  await fetch(endpoint + '/user', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          email: registerEmail.value,
-          name: registerName.value,
-          password: registerPassword.value
-        })
-      }
-  ).then(async (response) => {
-    registerData.value = await response.json()
-
-    // TODO: register should return 201
-    if (response.status === 201) {
-      if (registerData.value.token) {
-        appStore.setToken(registerData.value.token)
-      }
-      // Notification message
-      notification.value = {
-        message: 'Welcome to our platform! ',
-        status: 'success'
-      }
-
-      // redirect to home page after timeout
-      setTimeout(() => {
-        router.push('/')
-      }, 2000)
-    } else {
-      notification.value = {
-        message: 'Unable to Register the user \n Message Error: ' + registerData.value.message,
-        status: 'error'
-      }
-    }
-  }).catch((error) => {
-    registerData.value = error
-    notification.value = {
-      message: 'Unable to Register the user \n Message Error: ' + error.message,
-      status: 'error'
-    }
-  })
-  // reset inputs values
-  registerEmail.value = ''
-  registerName.value = ''
-  registerPassword.value = ''
+  // email.value = ''
+  // password.value = ''
+  // email.value = 'done'
 }
 </script>
