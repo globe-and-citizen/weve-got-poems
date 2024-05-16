ffbda519af87011e77818bd9c8f4a13892490682
achille <achillegley@gmail.com>
2024-05-16 09:14:09 +0700

    update quasar config for production

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -9,7 +9,7 @@
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
 
 const { configure } = require('quasar/wrappers');
-
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 module.exports = configure(function (/* ctx */) {
   return {
     // https://v2.quasar.dev/quasar-cli-vite/prefetch-feature
